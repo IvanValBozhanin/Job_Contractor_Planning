0f842e5f27b85f62035d84728c21e2185bb00711
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:42:35 +0100

    Added JavaDoc documentation.

--- src/Date.java
+++ src/Date.java
@@ -6,21 +6,49 @@ public class Date {
     private int month;
     private int year;
 
+    /**
+     * Constructor of the Date object
+     * @param day - the day of the date
+     * @param month - the month of the date
+     * @param year - the year of the date
+     */
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
-
+    /**
+     * Constructor of the Date object from a string to read
+     * @param nextLine - the line with info
+     */
     public Date(String nextLine) {
         Scanner in = new Scanner(nextLine);
         in.useDelimiter("-");
         this.day = in.nextInt();
         this.month = in.nextInt();
         this.year = in.nextInt();
-
     }
 
+    /**
+     * Method for scanning the date from a line of text
+     * @param in - the string to be scanned
+     * @return a new date with info from the field
+     */
+    public static Date scanDate(Scanner in) {
+        System.out.print("Day: ");
+        int day = in.nextInt();
+        System.out.print("Month: ");
+        int month = in.nextInt();
+        System.out.print("Year: ");
+        int year = in.nextInt();
+        return new Date(day, month, year);
+    }
+
+    /**
+     * Equals method of Date
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -29,11 +57,19 @@ public class Date {
         return day == date.day && month == date.month && year == date.year;
     }
 
+    /**
+     * Hashcode method of the Date object
+     * @return integer of the hascode of the object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(day, month, year);
     }
 
+    /**
+     * ToString method of Date
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "Date{" +
@@ -43,26 +79,50 @@ public class Date {
                 '}';
     }
 
+    /**
+     * Getter of the day field
+     * @return the day of the date
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     * Getter of the month field
+     * @return the month of the date
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * Getter of the year field
+     * @return the year of the date
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * Setter of the day field
+     * @param day - the new day to be set
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     * Setter of the month field
+     * @param month  - the new day to be set
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * Setter of the year field
+     * @param year - the new day to be set
+     */
     public void setYear(int year) {
         this.year = year;
     }
--- src/JackHammer.java
+++ src/JackHammer.java
@@ -1,19 +1,36 @@
 public class JackHammer extends Equipment implements PowerSupply{
 
+    /**
+     * Constructor of the Jack Hammer object that takes only list of requirements
+     * @param requirements - the list of requirements
+     */
     public JackHammer(String requirements) {
         super(requirements);
     }
 
+    /**
+     * Equals method of Jack Hammer
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
         return super.equals(o) ;//&& this.getClass() == o.getClass();
     }
 
+    /**
+     * ToString method of Jack Hammer
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "JackHammer{" + super.toString() + " " + getPowerSupply() + "}";
     }
 
+    /**
+     * Getter of the type of power supply
+     * @return - the power supply needed to use the Jack Hammer
+     */
     @Override
     public String getPowerSupply() {
         return "Air Compressor power supply";
