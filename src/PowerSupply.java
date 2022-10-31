aec4479828778d3901a9929d917fd1ec953fe728
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:39:52 +0100

    Added JavaDoc documentation.

--- src/PowerSupply.java
+++ src/PowerSupply.java
@@ -1,4 +1,8 @@
 public interface PowerSupply {
 
+    /**
+     * Getter of the power supply
+     * @return the string of power supply needed for the equipment
+     */
     String getPowerSupply();
 }
