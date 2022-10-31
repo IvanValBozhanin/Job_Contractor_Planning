0c054bede3f9021fd70c70201ab78da51f099032
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:42:31 +0100

    Added JavaDoc documentation.

--- src/Address.java
+++ src/Address.java
@@ -7,6 +7,13 @@ public class Address {
     private String zipCode;
     private String city;
 
+    /**
+     * Constructor for the Address object via passing all values
+     * @param street - the name of the street
+     * @param number - the number of the street
+     * @param zipCode - the ZIP code of the address
+     * @param city - the city of address
+     */
     public Address(String street, int number, String zipCode, String city) {
         this.street = street;
         this.number = number;
@@ -14,8 +21,12 @@ public class Address {
         this.city = city;
     }
 
-    public Address(String street) {
-        Scanner in = new Scanner(street);
+    /**
+     * Constructor for the Address object via passing a line with all data.
+     * @param data - the string with the data
+     */
+    public Address(String data) {
+        Scanner in = new Scanner(data);
         in.useDelimiter("; ");
         this.street = in.next();
         this.number = in.nextInt();
@@ -23,6 +34,11 @@ public class Address {
         this.city = in.next();
     }
 
+    /**
+     * Equals method for Address
+     * @param o - the object to be compared to this address
+     * @return true iff all the fields are the same in value
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -31,11 +47,19 @@ public class Address {
         return number == address.number && Objects.equals(zipCode, address.zipCode);
     }
 
+    /**
+     * Hashcode method for the Address object
+     * @return the integer of the hashcode of the address's instance
+     */
     @Override
     public int hashCode() {
         return Objects.hash(street, number, zipCode, city);
     }
 
+    /**
+     * ToString method for Address
+     * @return a human-readable format of the object
+     */
     @Override
     public String toString() {
         return "Address{" +
@@ -46,18 +70,34 @@ public class Address {
                 '}';
     }
 
+    /**
+     * Getter of street's name
+     * @return the street's name
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Getter of the street's number
+     * @return the street's number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * Getter of the ZIP code of the address
+     * @return the address's ZIP code
+     */
     public String getZipCode() {
         return zipCode;
     }
 
+    /**
+     * Getter of the City field
+     * @return the name of the address's city
+     */
     public String getCity() {
         return city;
     }
--- src/ConcreteMixer.java
+++ src/ConcreteMixer.java
@@ -1,16 +1,29 @@
 public class ConcreteMixer extends Equipment {
 
+    /**
+     * Constructor of the Concrete Mixer object that takes only list of requirements
+     * @param requirements - the list of requirements
+     */
     public ConcreteMixer(String requirements) {
         super(requirements);
     }
 
+    /**
+     * ToString method of Concrete Mixer
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "ConcreteMixer{ " + super.toString() + " }";
     }
 
+    /**
+     * Equals method of Concrete Mixer
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
-        return super.equals(o);// && this.getClass() == o.getClass();
+        return super.equals(o);
     }
 }
