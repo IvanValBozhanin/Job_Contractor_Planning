f28f8e3bb8c6b501169f30a5a8354bd4bd3751c1
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:42:39 +0100

    Added JavaDoc documentation.

--- src/Equipment.java
+++ src/Equipment.java
@@ -3,14 +3,27 @@ import java.util.Objects;
 public abstract class Equipment {
     private String requirements;
 
+    /**
+     * Constructor for the equipment
+     * @param requirements - the requirements for the equipment
+     */
     public Equipment(String requirements) {
         this.requirements = requirements;
     }
 
+    /**
+     * Getter method of the requirements for the equipment
+     * @return the string of the requirements for the equipment
+     */
     public String getRequirements() {
         return requirements;
     }
 
+    /**
+     * Equals method of Equipment
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -20,12 +33,14 @@ public abstract class Equipment {
         return Objects.equals(requirements, equipment.requirements);
     }
 
+    /**
+     * ToString method of Equipment
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "Equipment{" +
                 "requirements='" + requirements + '\'' +
                 '}';
     }
-
-
 }
