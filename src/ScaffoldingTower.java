384eb260ce30c35590bef9115e061488a40fe9a2
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:39:55 +0100

    Added JavaDoc documentation.

--- src/ScaffoldingTower.java
+++ src/ScaffoldingTower.java
@@ -1,13 +1,27 @@
 public class ScaffoldingTower extends Equipment{
+
+    /**
+     * Constructor of the Scaffolding Tower object that takes only list of requirements
+     * @param requirements - the list of requirements
+     */
     public ScaffoldingTower(String requirements) {
         super(requirements);
     }
 
+    /**
+     * Equals method of Scaffolding Tower
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
         return super.equals(o);// && getClass() == o.getClass();
     }
 
+    /**
+     * ToString method of Scaffolding Tower
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "ScaffoldingTower{" + super.toString() + "}";
