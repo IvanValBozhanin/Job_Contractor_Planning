eafda6d9a890666a4504c29405cda97ecf137306
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-10-31 21:42:23 +0100

    Added JavaDoc documentation.

--- src/Job.java
+++ src/Job.java
@@ -11,7 +11,15 @@ public class Job {
     private static int jobTotal = 0;
     protected static ArrayList<Job> listOfJobs = new ArrayList<>();
 
-    public Job(Address location, String description, List<Equipment> requiredEquipment, Date plannedDate) {
+    /**
+     * Constructor for the Job object
+     * @param location - the address of the job
+     * @param description - the description of the job
+     * @param requiredEquipment - the required equipment of the job
+     * @param plannedDate - the planned date of the job
+     */
+    public Job(Address location, String description,
+               List<Equipment> requiredEquipment, Date plannedDate) {
         this.location = location;
         this.description = description;
         this.requiredEquipment = requiredEquipment;
@@ -19,19 +27,35 @@ public class Job {
         this.jobNumber = ++jobTotal;
     }
 
+    /**
+     * Equals method of Job
+     * @param o - the object to be compared
+     * @return true iff the object is equal in terms of fields and class
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Job job = (Job) o;
-        return Objects.equals(location, job.location) && Objects.equals(description, job.description) && Objects.equals(requiredEquipment, job.requiredEquipment) && Objects.equals(plannedDate, job.plannedDate);
+        return Objects.equals(location, job.location) &&
+                Objects.equals(description, job.description) &&
+                Objects.equals(requiredEquipment, job.requiredEquipment)
+                && Objects.equals(plannedDate, job.plannedDate);
     }
 
+    /**
+     * Hashcode method of the Job object
+     * @return integer of the hascode of the object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(jobNumber, location, description, requiredEquipment, plannedDate);
     }
 
+    /**
+     * ToString method of Job
+     * @return a string of info in human-readable format
+     */
     @Override
     public String toString() {
         return "Job{" +
@@ -43,27 +67,43 @@ public class Job {
                 '}';
     }
 
+    /**
+     * Getter of the job number
+     * @return the integer of the job number
+     */
     public int getJobNumber() {
         return jobNumber;
     }
 
+    /**
+     * Getter of the address of the job
+     * @return the address of the job
+     */
     public Address getLocation() {
         return location;
     }
 
+    /**
+     * Getter for the description of the job
+     * @return the description of the job
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Getter for the required equipment of the job
+     * @return the required equipment of the job
+     */
     public List<Equipment> getRequiredEquipment() {
         return requiredEquipment;
     }
 
+    /**
+     * Getter for the planned date of the job
+     * @return the planned date of the job
+     */
     public Date getPlannedDate() {
         return plannedDate;
-    }
-
-    public static int getJobTotal() {
-        return jobTotal;
     }
 }
