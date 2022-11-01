a84c981598e0551350794679629fec0ea41df841
Ivan Val. Bozhanin <ivan156drs@gmail.com>
2022-11-01 23:56:44 +0100

    Added JUnit Tests to reach 100% line coverage.

--- test/EquipmentTest.java
+++ test/EquipmentTest.java
@@ -6,16 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class EquipmentTest {
 
     private Equipment concrete;
+    private Equipment concrete1;
     private Equipment torch;
+    private Equipment torch1;
     private Equipment tower;
+    private Equipment tower1;
     private Equipment hammer;
+    private Equipment hammer1;
 
     @BeforeEach
     void setUp() {
         concrete = new ConcreteMixer("Siuuuu");
+        concrete1 = new ConcreteMixer("Siuuuu");
         torch = new Torch("Siuuuu");
+        torch1 = new Torch("Siuuuu");
         tower = new ScaffoldingTower("Siuuuu");
+        tower1 = new ScaffoldingTower("Siuuuu");
         hammer = new JackHammer("Siuuuu");
+        hammer1 = new JackHammer("Siuuuu");
     }
 
     @Test
@@ -24,12 +32,18 @@ class EquipmentTest {
     }
 
     @Test
-    void testEqualsFailure() {
-        assertNotEquals(concrete, tower);
+    void testEqualsSuccess() {
+        assertEquals(concrete, concrete1);
+        assertEquals(hammer, hammer1);
+        assertEquals(tower, tower1);
+        assertEquals(torch, torch1);
     }
 
     @Test
     void testToStringSuccess() {
-        assertEquals("Torch{Equipment{requirements='Siuuuu'}}", torch.toString());
+        assertEquals("Torch{Equipment{requirements='Siuuuu'} Butane Gas power supply}", torch.toString());
+        assertEquals("ScaffoldingTower{Equipment{requirements='Siuuuu'}}", tower.toString());
+        assertEquals("JackHammer{Equipment{requirements='Siuuuu'} Air Compressor power supply}", hammer.toString());
+        assertEquals("ConcreteMixer{ Equipment{requirements='Siuuuu'} }", concrete.toString());
     }
 }
